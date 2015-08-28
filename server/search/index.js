ece07250e7cc3c222f8d032876085f9097fb37f3
Saul Maddox <saul.maddox@gmail.com>
2015-08-27 20:12:14 -0500

    add comments on where to get github data

--- server/search/index.js
+++ server/search/index.js
@@ -14,8 +14,8 @@ exports.register = function(server, options, next){
                     keywords: [Joi.string(), Joi.array(Joi.string())],
                     license: Joi.string(),
                     weights: Joi.object().keys({
-                        age: Joi.number().integer().min(0).max(10).default(5),
-                        commitLast: Joi.number().integer().min(0).max(10).default(5),
+                        age: Joi.number().integer().min(0).max(10).default(5), // GITHUB https://api.github.com/repos/user/repo created_at
+                        commitLast: Joi.number().integer().min(0).max(10).default(5), // GITHUB https://api.github.com/repos/user/repo updated_at
                         commitsPerContributor: Joi.number().integer().min(0).max(10).default(5),
                         commitsQuantity: Joi.number().integer().min(0).max(10).default(5),
                         dependencies: Joi.number().integer().min(0).max(10).default(5),
@@ -23,15 +23,16 @@ exports.register = function(server, options, next){
                         downloadsDay: Joi.number().integer().min(0).max(10).default(5),
                         downloadsMonth: Joi.number().integer().min(0).max(10).default(5),
                         downloadsWeek: Joi.number().integer().min(0).max(10).default(5),
-                        forks: Joi.number().integer().min(0).max(10).default(5),
-                        issuesOpen: Joi.number().integer().min(0).max(10).default(5),
+                        forks: Joi.number().integer().min(0).max(10).default(5), // GITHUB https://api.github.com/repos/user/repo forks
+                        issuesOpen: Joi.number().integer().min(0).max(10).default(5), // GITHUIB https://api.github.com/repos/user/repo open_issues_count
                         issuesQuantiy: Joi.number().integer().min(0).max(10).default(5),
                         issuesResponseTime: Joi.number().integer().min(0).max(10).default(5),
                         issuesThreadLength: Joi.number().integer().min(0).max(10).default(5),
                         pullRequestsOpen: Joi.number().integer().min(0).max(10).default(5),
                         releases: Joi.number().integer().min(0).max(10).default(5),
-                        stars: Joi.number().integer().min(0).max(10).default(5),
-                        tests: Joi.number().integer().min(0).max(10).default(5)
+                        stars: Joi.number().integer().min(0).max(10).default(5), // GITHUB https://api.github.com/repos/user/repo stargazers_count
+                        tests: Joi.number().integer().min(0).max(10).default(5),
+                        watchers: Joi.number().integer().min(0).max(10).default(5) // GITHUB https://api.github.com/repos/user/repo subscribers_count
                     })
                 }
             },
