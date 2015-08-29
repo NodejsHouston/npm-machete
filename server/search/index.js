10e18542a00d9c67fe49952ef02003ea90bb18e2
Saul Maddox <saul.maddox@gmail.com>
2015-08-29 17:20:27 -0500

    Added Elasticsearch with filters and added tests.
    Added search ability through Elasticsearch with filters.
    Added tests for the search api.
    Added enviornment variable loader through the .env files.

--- server/search/index.js
+++ server/search/index.js
@@ -1,43 +1,113 @@
+/**
+* Dependencies.
+*/
 var Joi = require('joi');
+var Boom = require('boom');
+var elasticsearch = require('elasticsearch');
 
 exports.register = function(server, options, next){
 
+    var elasticsearchClient = new elasticsearch.Client({
+        host: process.env.ELASTICSEARCH_URL
+    });
+
     server.route({
         method: 'GET',
-        path: '/search',
+        path: '/api/1/search',
         config: {
             id: 'search',
             validate: {
                 query: {
-                    q: Joi.string(),
-                    author: [Joi.string(), Joi.array(Joi.string())],
-                    keywords: [Joi.string(), Joi.array(Joi.string())],
-                    license: Joi.string(),
-                    weights: Joi.object().keys({
-                        age: Joi.number().integer().min(0).max(10).default(5), // GITHUB https://api.github.com/repos/user/repo created_at
-                        commitLast: Joi.number().integer().min(0).max(10).default(5), // GITHUB https://api.github.com/repos/user/repo updated_at
-                        commitsPerContributor: Joi.number().integer().min(0).max(10).default(5),
-                        commitsQuantity: Joi.number().integer().min(0).max(10).default(5),
-                        dependencies: Joi.number().integer().min(0).max(10).default(5),
-                        dependents: Joi.number().integer().min(0).max(10).default(5),
-                        downloadsDay: Joi.number().integer().min(0).max(10).default(5),
-                        downloadsMonth: Joi.number().integer().min(0).max(10).default(5),
-                        downloadsWeek: Joi.number().integer().min(0).max(10).default(5),
-                        forks: Joi.number().integer().min(0).max(10).default(5), // GITHUB https://api.github.com/repos/user/repo forks
-                        issuesOpen: Joi.number().integer().min(0).max(10).default(5), // GITHUIB https://api.github.com/repos/user/repo open_issues_count
-                        issuesQuantiy: Joi.number().integer().min(0).max(10).default(5),
-                        issuesResponseTime: Joi.number().integer().min(0).max(10).default(5),
-                        issuesThreadLength: Joi.number().integer().min(0).max(10).default(5),
-                        pullRequestsOpen: Joi.number().integer().min(0).max(10).default(5),
-                        releases: Joi.number().integer().min(0).max(10).default(5),
-                        stars: Joi.number().integer().min(0).max(10).default(5), // GITHUB https://api.github.com/repos/user/repo stargazers_count
-                        tests: Joi.number().integer().min(0).max(10).default(5),
-                        watchers: Joi.number().integer().min(0).max(10).default(5) // GITHUB https://api.github.com/repos/user/repo subscribers_count
-                    })
+                    q: Joi.string().lowercase(),
+                    author: [Joi.string().lowercase(), Joi.array().items(Joi.string().lowercase())],
+                    keyword: [Joi.string().lowercase(), Joi.array().items(Joi.string().lowercase())],
+                    license: [Joi.string().lowercase(), Joi.array().items(Joi.string().lowercase())],
                 }
             },
             handler: function(request, reply){
-                reply(request.query);
+
+                var query = request.query.q;
+                var author = request.query.author;
+                var keyword = request.query.keyword;
+                var license = request.query.license;
+
+                var elasticsearchQuery = {
+                    filtered: {
+                    }
+                };
+
+                if (query) {
+                    elasticsearchQuery.filtered.query = {
+                        multi_match: {
+                            query: request.query.q,
+                            fields: ['name', 'description', 'readme']
+                        }
+                    }
+                }
+
+                if (author || keyword || license) {
+
+                    elasticsearchQuery.filtered.filter = {
+                        and: [
+                        ]
+                    };
+
+                    var filters = elasticsearchQuery.filtered.filter.and;
+
+                    if (author) {
+                        var authors = [].concat(author);
+                        if (authors.length > 1) {
+                            filters.push({ terms: { author: authors } });
+                        } else {
+                            filters.push({ term: { author: authors[0] } });
+                        }
+                    }
+
+                    if (keyword) {
+                        var keywords = [].concat(keyword);
+                        if (keywords.length > 1) {
+                            filters.push({ terms: { keywords: keywords } });
+                        } else {
+                            filters.push({ term: { keywords: keywords[0] } });
+                        }
+                    }
+
+                    if (license) {
+                        var licenses = [].concat(license);
+                        if (licenses.length > 1) {
+                            filters.push({ terms: { license: licenses } });
+                        } else {
+                            filters.push({ term: { license: licenses[0] } });
+                        }
+                    }
+
+                }
+
+                elasticsearchClient.search({
+                    index: 'npmmachete',
+                    size: 100,
+                    body: {
+                        query: elasticsearchQuery,
+                        sort: { _score: 'desc'}
+                    }
+                }).then(function (body) {
+
+                    var response = {
+                        results: []
+                    };
+
+                    body.hits.hits.forEach(function(item, index){
+                        response.results.push(item._source);
+                    });
+
+                    reply(response);
+
+                }).catch(function (error) {
+                    reply(Boom.badData());
+                    request.log('error', error.message);
+                    console.trace(error.message);
+                });
+
             }
         }
     });
